8e0e2c72465248131db6087074c92de6f56ab56f
Sean <18058799212@163.com>
2017-11-16 19:56:59 +0800

    修改实现方式，实现方法链

--- src/arithmetic.js
+++ src/arithmetic.js
@@ -1,93 +1,99 @@
-/**
- * 利用字符串去除小数点
- * @param {*} arg 待处理的数据
- * @return {*} numString 去除小数点转化得到的整数
- * @return {*} decimalLen 数字精度
- */
-function numToString(arg) {
-  let l, numString;
-  try {
-    numString = arg.toString();
-    l = numString.split(".")[1].length;
-  } catch (e) {
-    l = 0;
-  }
 
-  return {
-    numString: Number(numString.replace(".", "")),
-    decimalLen: l
-  };
-}
+/**
+ * @author 和霁
+ * @date 2017-11-15
+ */
+
 
 class Arithmetic {
-  add(arg1, arg2) {
-    let argR1 = numToString(arg1);
-    let argR2 = numToString(arg2);
-
-    let num1 = argR1.numString,
-      l1 = argR1.decimalLen,
-      num2 = argR2.numString,
-      l2 = argR2.decimalLen;
-
-    let c = Math.abs(l1 - l2);
-    let m = Math.pow(10, Math.max(l1, l2));
-    //cm：俩数小数点位数之差*10
-    if (c > 0) {
-      var cm = Math.pow(10, c);
-      l1 > l2 ? (num2 = num2 * cm) : (num1 = num1 * cm);
-    }
-    return (num1 + num2) / m;
-  }
-
-  subtract(arg1, arg2) {
-    let argR1 = numToString(arg1);
-    let argR2 = numToString(arg2);
-
-    let num1 = argR1.numString,
-      l1 = argR1.decimalLen,
-      num2 = argR2.numString,
-      l2 = argR2.decimalLen;
-
-    let c = Math.abs(l1 - l2);
-    let m = Math.pow(10, Math.max(l1, l2));
-    //cm：俩数小数点位数之差*10
-    if (c > 0) {
-      var cm = Math.pow(10, c);
-      l1 > l2 ? (num2 = num2 * cm) : (num1 = num1 * cm);
+    constructor(num) {
+        this.value = num;
+        this.current = this._numToString(num);
     }
 
-    return (num1 - num2) / m;
-  }
+    /**
+     * 利用字符串去除小数点
+     * @param {*} arg 待处理的数据
+     * @return {*} numString 去除小数点转化得到的整数
+     * @return {*} decimalLen 数字精度
+     * -----------------------------------------------------------------
+     */
+    _numToString(arg) {
+        let l, numString;
+        try {
+            numString = arg.toString();
+            l = numString.split(".")[1].length;
+        } catch (e) {
+            l = 0;
+        }
 
-  multiply(arg1, arg2) {
-    let argR1 = numToString(arg1);
-    let argR2 = numToString(arg2);
+        return {
+            numString: Number(numString.replace(".", "")),
+            decimalLen: l
+        };
+    }
 
-    let num1 = argR1.numString,
-      l1 = argR1.decimalLen,
-      num2 = argR2.numString,
-      l2 = argR2.decimalLen;
+    add(num) {
+        let next = this._numToString(num);
 
-    let m = l1 + l2;
+        const { numString: n1, decimalLen: l1 } = this.current;
+        const { numString: n2, decimalLen: l2 } = next;
 
-    return num1 * num2 / Math.pow(10, m);
-  }
+        let c = Math.abs(l1 - l2);
+        let m = Math.pow(10, Math.max(l1, l2));
+        //cm：俩数小数点位数之差*10
+        if (c > 0) {
+            var cm = Math.pow(10, c);
+            l1 > l2 ? (n2 = n2 * cm) : (n1 = n1 * cm);
+        }
+        this.value = (n1 + n2) / m;
 
-  divide(arg1, arg2) {
-    let argR1 = numToString(arg1);
-    let argR2 = numToString(arg2);
+        this.current = this._numToString(this.value);
 
-    let num1 = argR1.numString,
-      l1 = argR1.decimalLen,
-      num2 = argR2.numString,
-      l2 = argR2.decimalLen;
+        return this;
+    }
 
-    let m = l2 - l1;
-    let c = Math.abs(m)
-    return m >= 0
-      ? num1 * Math.pow(10, m) / num2
-      : num1 / (num2 * Math.pow(10, c));
-  }
+    sub(num) {
+        this.add(-num);
+    }
+
+    mul(num) {
+        let next = this._numToString(num);
+
+        const { numString: n1, decimalLen: l1 } = this.current;
+        const { numString: n2, decimalLen: l2 } = next;
+
+        let m = l1 + l2;
+
+        this.value = n1 * n2 / Math.pow(10, m);
+
+        this.current = this._numToString(this.value);
+
+        return this;
+    }
+
+    div(num) {
+        let next = this._numToString(num);
+
+        const { numString: n1, decimalLen: l1 } = this.current;
+        const { numString: n2, decimalLen: l2 } = next;
+
+        let m = l2 - l1;
+        let c = Math.abs(m);
+
+        this.value =
+            m >= 0 ? n1 * Math.pow(10, m) / n2 : n1 / (n2 * Math.pow(10, c));
+
+        this.current = this._numToString(this.value);
+
+        return this;
+    }
+
+    getValue() {
+        return this.value;
+    }
 }
 
-module.exports = new Arithmetic();
+export default Arithmetic;
+
+// module.exports = new Arithmetic();
